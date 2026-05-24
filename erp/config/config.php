33df28f253b9bbbff4179e42a876cc40d58470df
Fadi <fadialyoussef586@gmail.com>
2026-05-24 23:01:45 +0400

    fix currency symbol constant replication

--- erp/config/config.php
+++ erp/config/config.php
@@ -14,25 +14,33 @@ define('DB_USER', getenv('DB_USER') ?: 'root');
 define('DB_PASSWORD', getenv('DB_PASSWORD') ?: '');
 define('DB_CHARSET', 'utf8mb4');
 
-// BASE_URL: من Environment أو تلقائي من مسار المشروع
-$baseUrl = getenv('BASE_URL') ?: '';
-if ($baseUrl !== '') {
-    define('BASE_URL', rtrim($baseUrl, '/'));
-} else {
-    $docRoot = str_replace('\\', '/', realpath($_SERVER['DOCUMENT_ROOT'] ?? '') ?: '');
-    $erpRoot = str_replace('\\', '/', realpath(BASE_PATH) ?: BASE_PATH);
-    $path = '';
-    if ($docRoot !== '' && str_starts_with($erpRoot, $docRoot)) {
-        $path = substr($erpRoot, strlen($docRoot));
+// BASE_URL: from environment or auto-detected from project path
+if (!defined('BASE_URL')) {
+    $baseUrl = getenv('BASE_URL') ?: '';
+    if ($baseUrl !== '') {
+        define('BASE_URL', rtrim($baseUrl, '/'));
+    } else {
+        $docRoot = str_replace('\\', '/', realpath($_SERVER['DOCUMENT_ROOT'] ?? '') ?: '');
+        $erpRoot = str_replace('\\', '/', realpath(BASE_PATH) ?: BASE_PATH);
+        $path = '';
+        if ($docRoot !== '' && str_starts_with($erpRoot, $docRoot)) {
+            $path = substr($erpRoot, strlen($docRoot));
+        }
+        define('BASE_URL', rtrim(str_replace('\\', '/', $path), '/'));
     }
-    define('BASE_URL', rtrim(str_replace('\\', '/', $path), '/'));
 }
 
 date_default_timezone_set('Asia/Riyadh');
 
-define('CURRENCY_CODE', 'USD');
-define('CURRENCY_SYMBOL', '$');
-define('APP_LANG', 'en');
+if (!defined('CURRENCY_CODE')) {
+    define('CURRENCY_CODE', 'USD');
+}
+if (!defined('CURRENCY_SYMBOL')) {
+    define('CURRENCY_SYMBOL', '$');
+}
+if (!defined('APP_LANG')) {
+    define('APP_LANG', 'en');
+}
 
 // شعار الشركة: ملف محلي في assets/img/ أو رابط COMPANY_LOGO_URL
 define('COMPANY_LOGO_URL', getenv('COMPANY_LOGO_URL') ?: '');
